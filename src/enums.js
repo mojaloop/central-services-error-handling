190f8fa75cc0f7b24263982222b2b7c8315e4b97
Miguel de Barros <miguel@debarros.me>
2019-07-31 11:19:08 +0100

    Added some additional functionality to Error Framework (#36)
    * Added some additional functionality to Error Framework:
    - Added Enum type for FSPIOPErrorTypes which include a description and associated regex.
    - Added Types on to existing Error Enums which contain a description and a regex.
    - Added method to find FSPIOPErrorCodes from a numerical or string code.
    - Added tests to find FSPIOPErrorCodes from a numerical or string code functionality.
    - Added a 'name' param to the types for ErrorCode Enums
    - Made the createFSPIOPError throw an INTERNAL_SERVER_ERROR when the apiErrorCode is undefined.
    - findFSPIOPErrorCode now using superfast hash lookup
    - Added 'name' to the FSPIOPErrorCodes enums. This is useful for comparisons (i.e. deep-equal, etc)
    - Overloaded the FSPIOPError constructor to take in an additional parameter `useMessageAsDescription` to support creating FSPIOPError objects from raw errorInformation objects
    - Added the 'cause' to be automatically added as an extension.
    - Added createFSPIOPErrorFromErrorInformation - Factory method to create an FSPIOPError based on the errorInformation object being passed in.
    - Added validateFSPIOPErrorCode - Validate a code against the Mojaloop API spec, returns the enum or throws an exception if invalid
    - Cleaned up some of the error handling framework factory code
    - Fixed typo in error message.
    
    * Updated to latest dependencies:
    - Have to bump the version up because I accidentally published the snapshot release as 7.2.0 instead of 7.2.0-snapshot
    - Also updating package-lock

--- src/enums.js
+++ src/enums.js
@@ -22,15 +22,130 @@
  * Gates Foundation
  - Name Surname <name.surname@gatesfoundation.com>
 
- * Neal Donnan <neal.donnan@modusbox.com>
+ * ModusBox
+ - Neal Donnan <neal.donnan@modusbox.com>
+ - Juan Correa <juan.correa@modusbox.com>
+ - Miguel de Barros <miguel.debarros@modusbox.com>
 
  --------------
  ******/
 
 'use strict'
 
-const Errors = require('@modusbox/mojaloop-sdk-standard-components').Errors
+const _ = require('lodash')
+
+const MojaloopSDKError = require('@modusbox/mojaloop-sdk-standard-components').Errors
+
+/**
+ *  Mojaloop API spec error type enums
+ */
+const MojaloopTypes = {
+  GENERIC_COMMUNICATION_ERROR: {
+    regex: '^10[0-9]{2}$',
+    description: 'Generic Communication Error'
+  },
+  GENERIC_SERVER_ERROR: {
+    regex: '^20[0-9]{2}$',
+    description: 'Generic Server Error'
+  },
+  GENERIC_CLIENT_ERROR: {
+    regex: '^30[0-9]{2}$',
+    description: 'Generic Client Error'
+  },
+  CLIENT_VALIDATION_ERROR: {
+    regex: '^31[0-9]{2}$',
+    description: 'Client Validation Error'
+  },
+  IDENTIFIER_ERROR: {
+    regex: '^32[0-9]{2}$',
+    description: 'Identifier Error'
+  },
+  EXPIRED_ERROR: {
+    regex: '^33[0-9]{2}$',
+    description: 'Expired Error'
+  },
+  PAYER_ERROR: {
+    regex: '^4[0-9]{3}$',
+    description: 'Payer Error'
+  },
+  PAYEE_ERROR: {
+    regex: '^5[0-9]{3}$',
+    description: 'Payee Error'
+  }
+}
+
+/**
+ * Returns an object representing a Mojaloop API spec error code combined with error types enums
+ *
+ * @param errorCodes {object} - Mojaloop API spec error code enums
+ * @param errorTypes {object} - Mojaloop API spec error type enums
+ * @returns {object} - Object representing the Mojaloop API spec error enums with associated types
+ */
+const populateErrorTypes = (errorCodes, errorTypes) => {
+  const newErrorCodes = {}
+  for (const [errorCodeKey, errorCodeValue] of Object.entries(errorCodes)) {
+    for (const [errorTypeKey, errorTypeValue] of Object.entries(errorTypes)) {
+      const regExp = new RegExp(errorTypeValue.regex)
+      if (regExp.test(errorCodeValue.code)) {
+        const newErrorCodeValue = _.cloneDeep(errorCodeValue)
+        _.set(newErrorCodeValue, 'name', errorCodeKey)
+        _.set(newErrorCodeValue, 'type', errorTypeValue)
+        _.set(newErrorCodeValue, 'type.name', errorTypeKey)
+        _.set(newErrorCodes, errorCodeKey, newErrorCodeValue)
+      }
+    }
+  }
+  return newErrorCodes
+}
+
+/**
+ * Returns an object representing a Mojaloop API spec error code map using the error code as the key
+ *
+ * @param errorCodes {object} - Mojaloop API spec error code enums
+ * @returns {object} - Object representing a Mojaloop API Error map using the error code as the key with each record having the associated name
+ */
+const errorCodesToMap = (errorCodes) => {
+  const newErrorCodeMap = {}
+  for (const [errorCodeKey, errorCodeValue] of Object.entries(errorCodes)) {
+    const newErrorCodeValue = _.cloneDeep(errorCodeValue)
+    _.set(newErrorCodeValue, 'name', errorCodeKey)
+    _.set(newErrorCodeMap, errorCodeValue.code.toString(), newErrorCodeValue)
+  }
+  return newErrorCodeMap
+}
+
+/**
+ *  Mojaloop API spec error enums with associated type enums
+ */
+const FSPIOPErrorCodes = populateErrorTypes(MojaloopSDKError.MojaloopApiErrorCodes, MojaloopTypes)
+
+/**
+ *  Mojaloop API spec error map enums with associated type enums by error code as the key map
+ */
+const FSPIOPErrorCodeMap = errorCodesToMap(FSPIOPErrorCodes)
+
+/**
+ * Returns an object representing a Mojaloop API spec error object given its error code using super fast hash lookup
+ *
+ * @param code {number/string} - Mojaloop API spec error code (four digit integer as number or string)
+ * @returns {object} - Object representing the Mojaloop API spec error
+ */
+const findFSPIOPErrorCode = (code) => {
+  let ec
+  if (code) {
+    const stringCodeValue = code.toString()
+    ec = FSPIOPErrorCodeMap[stringCodeValue]
+  }
+
+  if (ec) {
+    return ec
+  }
+  return undefined
+}
 
 module.exports = {
-  FSPIOPErrorCodes: Errors.MojaloopApiErrorCodes
+  FSPIOPErrorCodes,
+  FSPIOPErrorTypes: MojaloopTypes,
+  FSPIOPErrorCodeMap,
+  findFSPIOPErrorCode
 }
