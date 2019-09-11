329f706f1c63dbb5030900ebed9ef9ba78a5139e
Matt Kingston <mattkingston@gmail.com>
2019-09-11 13:02:22 +0100

    Handling specific Joi validation failures (#67)
    * Added handling for specific Joi validation failures
    
    * Updated comment to hopefully trigger webhooks
    
    * Reverted previous buggy change
    
    * Some commenting
    
    * Updated comment

--- test/factory.test.js
+++ test/factory.test.js
@@ -133,6 +133,60 @@ Test('Factory should', factoryTest => {
     test.end()
   })
 
+  factoryTest.test('create an FSPIOPError from a Joi error and invalid path parameter response with toApiErrorObject includeCauseExtension: false, truncateCause: true', function (test) {
+    const joiError = {
+      type: 'any.allowOnly',
+      context: {
+        label: 'Type'
+      }
+    }
+    const joiResponse = {
+      output: {
+        payload: {
+          validation: {
+            source: 'params'
+          }
+        }
+      }
+    }
+    const fspiopError = Factory.createFSPIOPErrorFromJoiError(joiError, joiResponse, 'dfsp1')
+    test.ok(fspiopError)
+    test.deepEqual(fspiopError.toApiErrorObject(), {
+      errorInformation: {
+        errorCode: '3101',
+        errorDescription: 'Malformed syntax - \'Type\' URI path parameter'
+      }
+    })
+    test.end()
+  })
+
+  factoryTest.test('create an FSPIOPError from a Joi error and missing header response with toApiErrorObject includeCauseExtension: false, truncateCause: true', function (test) {
+    const joiError = {
+      type: 'any.required',
+      context: {
+        label: 'content-type'
+      }
+    }
+    const joiResponse = {
+      output: {
+        payload: {
+          validation: {
+            source: 'header'
+          }
+        }
+      }
+    }
+    const fspiopError = Factory.createFSPIOPErrorFromJoiError(joiError, joiResponse, 'dfsp1')
+    test.ok(fspiopError)
+    test.deepEqual(fspiopError.toApiErrorObject(), {
+      errorInformation: {
+        errorCode: '3102',
+        errorDescription: 'Missing mandatory element - \'content-type\' HTTP header'
+      }
+    })
+    test.end()
+  })
+
   factoryTest.test('create an FSPIOPError from a Joi error with toApiErrorObject includeCauseExtension: false, truncateCause: true', function (test) {
     const joiError = {
       type: 'any.required',
