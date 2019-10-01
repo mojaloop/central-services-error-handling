767d49cd416678384467633c0f7b259109373c44
Juan Correa <gibaros@users.noreply.github.com>
2019-10-01 13:52:13 -0700

    964-CreateHapiPluginToValidateErrorCodesOnPreHandler (#72)
    * 964-CreateHapiPluginToValidateErrorCodesOnPreHandler
    
    * 964-ValidateErrorCodes
    
    -add takeover() to onPreHandler plugin response
    -add toApiErrorObject()
    -instead of obtaining the response from the request object, obtain the payload and specifically its error code
    -change boolean if validateFspiopErrorCode to null check
    
    * 964-ValidateIncomingErrorCodes
    -let code execution continue if valid mojaloop error code received
    -add dependency check and update scripts
    -add unit tests to cover onPreHandler function
    
    * 964-ValidateIncomingErrorCodes
    -add unit test to meet code coverage requirement
    
    * 964-ValidateIncomingErrorCodes
    -update unit test to cover 2 invalid error codes, for invalid category and invalid specific error
    
    * 964-FixVulnerabilityCheck
    -add .ncurc.yml
    -add prune run command
    
    * Issue964-AddAndConfigureNyc
    
    * Issue964-UpdateDependencies
    -remove istanbul
    -run update dep check
    -rename pretest to standard
    
    * Issue964-RenameAuditResolvToAuditResolve
    
    * Issue964-RenameOnPreHandlerToValidateIncomingErrorCode
    
    * RemovedUnusedSinonImport
    
    * Issue964-BumpRepoVersionTo8dot2dot0
    
    * Issue964-UpdateNcurcAndNycrc

--- test/handler.test.js
+++ test/handler.test.js
@@ -1,33 +1,33 @@
 /*****
-License
---------------
-  Copyright © 2017 Bill & Melinda Gates Foundation
-The Mojaloop files are made available by the Bill & Melinda Gates Foundation under the Apache License, Version 2.0 (the "License") and you may not use these files except in compliance with the License. You may obtain a copy of the License at
+ License
+ --------------
+ Copyright © 2017 Bill & Melinda Gates Foundation
+ The Mojaloop files are made available by the Bill & Melinda Gates Foundation under the Apache License, Version 2.0 (the "License") and you may not use these files except in compliance with the License. You may obtain a copy of the License at
 
-http://www.apache.org/licenses/LICENSE-2.0
+ http://www.apache.org/licenses/LICENSE-2.0
 
-  Unless required by applicable law or agreed to in writing, the Mojaloop files are distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+ Unless required by applicable law or agreed to in writing, the Mojaloop files are distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
 
-  Contributors
---------------
-  This is the official list of the Mojaloop project contributors for this file.
-  Names of the original copyright holders (individuals or organizations)
-should be listed with a '*' in the first column. People who have
-contributed from an organization can be listed under the organization
-that actually holds the copyright for their contributions (see the
-Gates Foundation organization for an example). Those individuals should have
-their names indented and be marked with a '-'. Email address can be added
-optionally within square brackets <email>.
+ Contributors
+ --------------
+ This is the official list of the Mojaloop project contributors for this file.
+ Names of the original copyright holders (individuals or organizations)
+ should be listed with a '*' in the first column. People who have
+ contributed from an organization can be listed under the organization
+ that actually holds the copyright for their contributions (see the
+ Gates Foundation organization for an example). Those individuals should have
+ their names indented and be marked with a '-'. Email address can be added
+ optionally within square brackets <email>.
 
-* Gates Foundation
-- Name Surname <name.surname@gatesfoundation.com>
+ * Gates Foundation
+ - Name Surname <name.surname@gatesfoundation.com>
 
-* ModusBox
-- Neal Donnan <neal.donnan@modusbox.com>
-- Rajiv Mothilal <rajiv.mothilal@modusbox.com>
+ * ModusBox
+ - Neal Donnan <neal.donnan@modusbox.com>
+ - Rajiv Mothilal <rajiv.mothilal@modusbox.com>
 
---------------
-******/
+ --------------
+ ******/
 
 'use strict'
 
@@ -39,15 +39,15 @@ const Handler = require('../src/handler')
 
 Test('Handler should', handlerTest => {
   handlerTest.test('handle non error responses', async function (test) {
-    const response = { }
-    test.ok(Handler.onPreResponse({ response: response }, { continue: true }))
+    const response = {}
+    test.ok(Handler.onPreResponse({response: response}, {continue: true}))
     test.end()
   })
 
   handlerTest.test('handle FSPIOPError responses', async function (test) {
     const fspiopError = Factory.createFSPIOPError(Enums.FSPIOPErrorCodes.INTERNAL_SERVER_ERROR, 'Internal Error')
     const response = fspiopError
-    test.ok(Handler.onPreResponse({ response: response }, { continue: true }))
+    test.ok(Handler.onPreResponse({response: response}, {continue: true}))
     test.equal(response.output.statusCode, 500)
     test.equal(response.output.payload.errorInformation.errorCode, '2001')
     test.equal(response.output.payload.errorInformation.errorDescription, 'Internal server error - Internal Error')
@@ -57,7 +57,7 @@ Test('Handler should', handlerTest => {
   handlerTest.test('handle generic Error responses', async function (test) {
     const error = new Error('Test Error')
     const response = error
-    test.ok(Handler.onPreResponse({ response: response }, { continue: true }))
+    test.ok(Handler.onPreResponse({response: response}, {continue: true}))
     test.equal(response.output.statusCode, 500)
     test.equal(response.output.payload.errorInformation.errorCode, '2001')
     test.equal(response.output.payload.errorInformation.errorDescription, 'Internal server error - Test Error')
@@ -75,7 +75,7 @@ Test('Handler should', handlerTest => {
             }
         }
     }
-    Handler.onPreResponse({ response: response, headers: { 'fspiop-source': 'dfsp1' } }, {})
+    Handler.onPreResponse({response: response, headers: {'fspiop-source': 'dfsp1'}}, {})
     test.equal(response.output.payload.errorInformation.errorCode, '2001')
     test.end()
   })
@@ -83,7 +83,7 @@ Test('Handler should', handlerTest => {
   handlerTest.test('handle Boom generated errors', async function (test) {
     const response = Boom.badRequest('some bad parameters')
 
-    Handler.onPreResponse({ response }, {})
+    Handler.onPreResponse({response}, {})
     test.equal(response.output.statusCode, 400)
     test.equal(response.output.payload.errorInformation.errorCode, '3000')
     test.equal(response.output.payload.errorInformation.errorDescription, 'Generic client error - some bad parameters')
@@ -93,7 +93,7 @@ Test('Handler should', handlerTest => {
   handlerTest.test('handle a Boom 404 error', async function (test) {
     const response = Boom.notFound('Not Found')
 
-    Handler.onPreResponse({ response }, {})
+    Handler.onPreResponse({response}, {})
     test.equal(response.output.statusCode, 404)
     test.equal(response.output.payload.errorInformation.errorCode, '3002')
     test.equal(response.output.payload.errorInformation.errorDescription, 'Unknown URI - Not Found')
@@ -111,9 +111,113 @@ Test('Handler should', handlerTest => {
         }
       }]
     }
-    Handler.onPreResponse({ response: response }, {})
+    Handler.onPreResponse({response: response}, {})
     test.equal(response.output.payload.errorInformation.errorDescription, 'Malformed syntax - Regular expression failed')
     test.equal(response.output.payload.errorInformation.errorCode, '3101')
+    test.end()
+  })
+
+  handlerTest.test('handle incoming valid mojaloop specification error code', async function (test) {
+    const payload =
+      {
+        errorInformation:
+          {
+            errorCode: '5105',
+            errorDescription: 'Payee transaction limit reached',
+            extensionList:
+              {
+                extension:
+                  [{
+                    key: 'errorDetail',
+                    value: 'This is an abort extension'
+                  }]
+              }
+          }
+      }
+    test.equal(Handler.validateIncomingErrorCode({ payload: payload }, { continue: payload }), payload)
+    test.end()
+  })
+
+  handlerTest.test('handle incoming mojaloop specification error code with invalid category', async function (test) {
+    const payload =
+      {
+        errorInformation:
+          {
+            errorCode: '15105',
+            errorDescription: 'Payee transaction limit reached',
+            extensionList:
+              {
+                extension:
+                  [{
+                    key: 'errorDetail',
+                    value: 'This is an abort extension'
+                  }]
+              }
+          }
+      }
+
+    const takeoverMessage =
+      {
+        errorInformation: {
+          errorCode: '3100',
+          errorDescription: 'Generic validation error - The incoming error code: 15105 is not a valid mojaloop specification error code'
+        }
+      }
+
+    const h = {
+      response: () => {
+        return {
+          code: () => {
+            return {
+              takeover: () => { return takeoverMessage }
+            }
+          }
+        }
+      }
+    }
+
+    test.equal(Handler.validateIncomingErrorCode({ payload: payload }, h), takeoverMessage)
+    test.end()
+  })
+
+  handlerTest.test('handle incoming mojaloop specification error code with valid high and low level category, but invalid specific error', async function (test) {
+    const payload =
+      {
+        errorInformation:
+          {
+            errorCode: '5199',
+            errorDescription: 'Payee transaction limit reached',
+            extensionList:
+              {
+                extension:
+                  [{
+                    key: 'errorDetail',
+                    value: 'This is an abort extension'
+                  }]
+              }
+          }
+      }
+    const takeoverMessage =
+      {
+        errorInformation: {
+          errorCode: '3100',
+          errorDescription: 'Generic validation error - The incoming error code: 15105 is not a valid mojaloop specification error code'
+        }
+      }
+
+    const h = {
+      response: () => {
+        return {
+          code: () => {
+            return {
+              takeover: () => { return takeoverMessage }
+            }
+          }
+        }
+      }
+    }
+
+    test.equal(Handler.validateIncomingErrorCode({ payload: payload }, h), takeoverMessage)
     test.end()
   })
   handlerTest.end()
